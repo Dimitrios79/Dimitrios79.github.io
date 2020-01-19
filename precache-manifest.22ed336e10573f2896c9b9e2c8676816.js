462ff3fb6610eb8ca1136ee55cc3bdc8675df2ee
Dimitrios Kallimanis <42889879+Dimitrios79@users.noreply.github.com>
2020-01-19 15:02:31 +0100

    Updates

---
+++ precache-manifest.22ed336e10573f2896c9b9e2c8676816.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c0ec65f775b95f21937284b754371b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44bee1ebf1ce400af5f",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "92d60dfdb2597aa98dd1",
+    "url": "/static/css/main.33575798.chunk.css"
+  },
+  {
+    "revision": "f44bee1ebf1ce400af5f",
+    "url": "/static/js/2.84b5882a.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/static/js/2.84b5882a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92d60dfdb2597aa98dd1",
+    "url": "/static/js/main.394be617.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
